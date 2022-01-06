917db8cea5eb2cee60a22c2d2ad407b59d476e5a
khaoula <khaoulajabour4@gmail.com>
2022-01-06 01:49:54 -0500

    connect database

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -5,6 +6,17 @@ const app = express();
 //express middleware
 app.use(express.urlencoded({extended : false}));
 app.use(express.json());
+
+// connect the db
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'pwd',
+        database:'election',
+    },
+    console.log('Connected to the election database')
+);
 
 
 // Default response for any other request (Not Found)
