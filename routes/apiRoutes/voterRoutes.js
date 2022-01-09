af39075c7070b31541f05b47086a0792048b1764
khaoula <khaoulajabour4@gmail.com>
2022-01-08 21:11:33 -0500

    add post put delete routes

--- routes/apiRoutes/voterRoutes.js
+++ routes/apiRoutes/voterRoutes.js
@@ -36,4 +36,79 @@ router.get('/voters/:id', (req, res)=>{
     });
 });
 
+// add a voter
+router.post('/voters', ({body}, res)=>{
+    // Data validation
+    const errors = inputCheck(body, 'first_name', 'last_name', 'email');
+    if(errors){
+        res.status(400).json({error : errors});
+        return;
+    }
+    const sql = `insert into voters (first_name, last_name, email) values (?, ? ,?)`;
+    const params = [body.first_name, body.last_name, body.email];
+    db.query(sql, params, (err, result)=>{
+        if(err){
+            res.status(400).json({error : err.message});
+            return;
+        }
+        res.json({
+            message : 'success',
+            data : body
+        });
+    });
+
+});
+
+// update email address of a voter
+router.put('/voters/:id', (req, res)=>{
+    const errors = inputCheck(req.body, 'email');
+    if (errors) {
+        res.status(400).json({error : errors});
+        return;
+    }
+
+    const sql = `update voters set email = ? where id = ?`;
+    const params = [req.body.email, req.params.id];
+    
+    db.query(sql, params, (err, result)=>{
+        if (err) {
+            res.status(400).json({error : err.message});
+        }
+        else if (!result.affectedRows) {
+            res.json({message : 'Voter not found!'});
+        }
+        else{
+            res.json({
+                message : 'success',
+                data : req.body,
+                changes : result.affectedRows
+            });
+        }
+    });
+});
+
+//delete a voter
+router.delete('/voters/:id', (req, res)=>{
+    
+    const sql = `delete from voters where id = ?`;
+    const params = req.params.id;
+
+    db.query(sql, params, (err, result)=>{
+        if (err) {
+            res.status(400).json({error : err.message});
+        } else if (!result.affectedRows) {
+            res.json({message : 'Voter not found!'});
+        }
+        else {
+            res.json({
+                message : 'deleted',
+                changes : result.affectedRows,
+                id : params
+
+            });
+        }
+    });
+});
+
+
 module.exports = router;
